aa858d048e8f2b77805ec4473b585b96b51c5243
Nhanjk <tranchinhan20102021@gmail.com>
2024-06-21 12:50:07 +0700

    build(*): Testing glfw
    fix(CMakeLists.txt): wrong path for glfw

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 /*
 
-   ____ _                    ___                    ____ _     
-  / ___| |__   ___  ___ ___ / _ \ _ __   ___ _ __  / ___| |    
- | |   | '_ \ / _ \/ __/ __| | | | '_ \ / _ \ '_ \| |  _| |    
- | |___| | | |  __/\__ \__ \ |_| | |_) |  __/ | | | |_| | |___ 
-  \____|_| |_|\___||___/___/\___/| .__/ \___|_| |_|\____|_____|
-                                 |_|                           
+   ____ _                      ___                    ____ _     
+  / ___| |__   ___  ___ ___   / _ \ _ __   ___ _ __  / ___| |    
+ | |   | '_ \ / _ \/ __/ __| | | | | '_ \ / _ \ '_ \| |  _| |    
+ | |___| | | |  __/\__ \__ \ | |_| | |_) |  __/ | | | |_| | |___ 
+  \____|_| |_|\___||___/___/  \___/| .__/ \___|_| |_|\____|_____|
+                                   |_|                           
 
 -- Author: hxajk (hxajkzzz@gmail.com).
 -- Update: 2024-06-20 15:12:00 
@@ -16,6 +16,21 @@
 
 int main()
 {
-    printf("Hello world!");
+    
+    if(!glfwInit())
+        return -1;
+
+    GLFWwindow* window = glfwCreateWindow(1000,1000,"ChessOpenGL", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    };
     return 0;
 };
