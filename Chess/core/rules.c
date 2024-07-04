c5826d1763270913e26278b1f7edd67759a8c4db
Nhanjk <tranchinhan20102021@gmail.com>
2024-07-04 17:38:51 +0700

    feat(rules): Simple hightlight events for pawn.

--- Chess/core/rules.c
+++ Chess/core/rules.c
@@ -1,19 +1,13 @@
 /// @file rules.c 
 
 #include <Chess/core/rules.h>
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 
 static struct Board board; static struct Piece piece;
 
-bool is_select_possible_move(double w, double h)
-{
-        printf("%d \n", board.index);
-        return false;
-};
-
 bool is_select_pieces(struct Piece self, double w, double h)
 {
-         glfwGetCursorPos(window_get().handle, &w, &h);
+        glfwGetCursorPos(window_get().handle, &w, &h);
 
         h = glm_max(0,h);   
         w = glm_max(0,w);
@@ -22,10 +16,25 @@ bool is_select_pieces(struct Piece self, double w, double h)
 
         piece = self;
 
-        if(is_select_possible_move(w, h))
+        // A : Press
+        // B : Release
+
+        // A -> B return true
+        
+
+        if(glfwGetMouseButton(window_get().handle, GLFW_MOUSE_BUTTON_LEFT))
         {
-                return true;
+                if(glms_aabb_events(piece.buffer_position_data, piece.index, w, h, 576) && piece.index >= 8 && piece.index <= 15)
+                {
+                        return true;
+                };
+
+                if(glms_aabb_events(piece.buffer_position_data, piece.index, w, h, 576) && piece.index >= 24 && piece.index <= 32)
+                {
+                        return true;
+                };
         };
+
 
         return false;
 };
@@ -41,10 +50,27 @@ bool is_possible_moves(struct Board self, double w, double h)
 
         board = self;
 
-        if(is_select_possible_move(w, h))
-        {
-                return true;
-        };
+        if(glfwGetMouseButton(window_get().handle, GLFW_MOUSE_BUTTON_LEFT)){
+
+                for(int i = 8;i <= 15;i ++){
+                        if(glms_aabb_events(piece.buffer_position_data, i, w, h, 576)){
+                                if(board.index == 2*8 + (i-8)|| board.index == 3*8 + (i-8) )
+                                {
+                                        return true;
+                                };
+                        };
+                };
+
+                for(int i = 24;i <= 32;i ++){
+                        if(glms_aabb_events(piece.buffer_position_data, i, w, h, 576)){
+                                if(board.index == 6*8 + (i-24)|| board.index == 5*8 + (i-24) )
+                                {
+                                        return true;
+                                };
+                        };
+                };
+
+        }
 
         return false;
 };
